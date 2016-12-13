c07dedda7cf7ed16f5da201fae5c7a5404ec9b85
serg <zaswed76@gmail.com>
2016-12-13 22:12:04 +0200

    1

--- pyomo/paths.py
+++ pyomo/paths.py
@@ -1,9 +1,14 @@
 from os import path
 
 root = path.dirname(__file__)
+print(root)
+
+def css_file(name):
+    return path.join(root, 'gui/css', name)
 
 def dict_source(base_name):
     return path.join(root, 'resource/dictionaries/source', base_name)
 
 def dict_work(base_name):
     return path.join(root, 'resource/dictionaries/work', base_name)
+
