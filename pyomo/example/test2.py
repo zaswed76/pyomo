138b52130f9ae85df75eb9eb81ee126ea7bc6c64
serg <zaswed76@gmail.com>
2016-12-14 09:17:52 +0200

    1

--- pyomo/example/test2.py
+++ pyomo/example/test2.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*
 
-print(float('0.1'))
+iterable = list(range(11))
+count = 3
+
+
+def f(iterable, limit):
+    a = []
+    lst = []
+    for i in iterable:
+        lst.append(i)
+        if len(lst) == limit:
+            a.append(lst)
+            lst = lst.copy()
+            lst.clear()
+
+
+
+    return a
+
+print(f(iterable,  4))
+
+
+
 
 
 
