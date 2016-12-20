6b4e0254883536e23dc700da7ecd618ffe3c2193
Admin <zaswed76@gmail.com>
2016-12-21 00:38:55 +0200

    исправлена ошибка с нулевым результатом поиска

--- pyomo/omoword.py
+++ pyomo/omoword.py
@@ -21,10 +21,13 @@ def printer(iterable, columns, tablefmt='plain'):
     'plain', 'simple', 'grid', 'pipe', 'orgtbl', 'rst', 'mediawiki',
     'latex', 'latex_booktabs'.
      """
-    size = int(math.ceil(len(iterable) / columns))
-    iterable = [iterable[i:i+size] for i in range(0, len(iterable), size)]
-    dict_iter = {_: i for _, i in enumerate(iterable)}
-    print(tabulate(dict_iter, tablefmt=tablefmt))
+    if iterable:
+        size = int(math.ceil(len(iterable) / columns))
+        iterable = [iterable[i:i+size] for i in range(0, len(iterable), size)]
+        dict_iter = {_: i for _, i in enumerate(iterable)}
+        print(tabulate(dict_iter, tablefmt=tablefmt))
+    else:
+        print('ничего не найдено - надо понизить рейтинг')
 
 
 def arg_parser(cfg):
